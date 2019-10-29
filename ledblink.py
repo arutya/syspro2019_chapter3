ffba908edf0f80a6522fb9c7a07f1f633c9a101f
arutya <3j02igarashi@gmail.com>
2019-10-29 10:52:36 +0900

    LEDを点滅させるプログラムを作成した

---
+++ ledblink.py
@@ -0,0 +1,15 @@
+import time 
+import Rpi.GPIO as GPIO
+
+count = 0
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+
+while count < 10:
+    GPIO.output(14, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(14, GPIO.LOW)
+    time.sleep(1)
+    count = count + 1
+
+GPIO.cleanup()
